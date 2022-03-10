9d34d8e5ab21d809e00d9a659bff02dbb5eec246
Dodow <d.plathey@yahoo.fr>
2022-03-10 17:50:32 +0100

    Update Observer.php

--- app/code/community/FastMage/NewsletterCheckoutReviewStep/Model/Observer.php
+++ app/code/community/FastMage/NewsletterCheckoutReviewStep/Model/Observer.php
@@ -1,1 +1,31 @@
-<?php 
+<?php
+class FastMage_NewsletterCheckoutReviewStep_Model_Observer
+{
+
+    public function addCheckbox (Varien_Event_Observer $observer)
+    {
+        if ($observer->getBlock() instanceof Mage_Checkout_Block_Agreements
+            && false === (boolean)(int)Mage::getStoreConfig('advanced/modules_disable_output/FastMage_NewsletterCheckoutReviewStep')
+        ) {
+            $checked = $this->isCheckboxCheckedByDefault() ? 'checked="checked"' : '';
+            $html = $observer->getTransport()->getHtml();
+            $checkboxHtml = '<li><p class="agree">'
+                . '<input id="subscribe_newsletter" name="is_subscribed" checked="checked" value="1" '. $checked .'class="checkbox" type="checkbox" title="' . $this->__($this->getCheckboxLabelText() . '" />'
+                . '<label for="subscribe_newsletter">' . $this->__($this->getCheckboxLabelText()) . '</label>'
+                . '</p></li>';
+            $html = str_replace('</ol>', $checkboxHtml . '</ol>', $html);
+            $observer->getTransport()->setHtml($html);
+        }
+    }
+
+    public function subscribe (Varien_Event_Observer $observer)
+    {
+        $quote = $observer->getEvent()->getQuote();
+        if ($quote->getBillingAddress() && Mage::app()->getRequest()->getParam('is_subscribed', false)) {
+            $status = Mage::getModel('newsletter/subscriber')
+                ->setImportMode(true)
+                ->subscribe($quote->getBillingAddress()->getEmail());
+        }
+    }
+
+}
